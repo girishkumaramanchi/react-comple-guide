e3629aa296f4e898523fca230c83d0e6188c9e8e
Girish <girishkumaramanchi@gmail.com>
2021-01-06 15:54:06 +0530

    Reusing the components with dynamic data

--- src/App.js
+++ src/App.js
@@ -11,7 +11,11 @@ class App extends Component {
         <h1 className="App-title">Welcome to React</h1>
         <p>I'm React App</p>
         <p>I'm learning React</p>
-        <Person />
+        <button style={{color:'blue'}}>Switch Name</button>
+        <Person name="Tatsuya" age={18} />
+        <Person name="Miyuki" age="17" />
+        <Person name="Lina" age="17" >I'm Half American Half Japanese</Person>
+        <Person name="X" age="Unknown" />
         {/* <Person></Person> */}
         {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,12 @@
 import React from "react"
 
-const person = () => {
-    return <p>I'm a person</p>
+const person = (props) => {
+    return( 
+        <div>
+            <p>I'm {props.name} and I'm {props.age} years old. My lucky number is {Math.floor(Math.random() * 30)}</p>
+            <p>{props.children}</p>
+        </div>
+    )
 }
 
 export default person;
