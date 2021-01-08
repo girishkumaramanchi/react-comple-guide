699701f77bef59d6d6c35f405ce52ce9490cb894
Girish <girishkumaramanchi@gmail.com>
2021-01-08 22:55:01 +0530

    done in functional state

--- src/App.js
+++ src/App.js
@@ -1,43 +1,50 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Person from "./Person/Person"
 // import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  state = {
-    persons: [
-      { name:"Tatsuya", age:18 },
-      { name:"Miyuki", age:17 },
-      { name:"Lina", age:17 },
-      { name:"X", age:undefined }
-    ]
-  }
+const App = props => {
+    const [ personsState, updatedPersonsState] = useState(
+      {
+        persons: [
+          { name:"Tatsuya", age:18 },
+          { name:"Miyuki", age:17 },
+          { name:"Lina", age:17 },
+          { name:"X", age:undefined }
+        ],
+        // otherState: "some other value"
+      }
+    );
 
-  switchNameHandler = () => {
-    // console.log("name changed");
-    // Don't do this: this.state.persons[0].name = "Shiba Tatsuya";
-    this.setState({
-      persons: [
-        { name:"Shiba Tatsuya", age:18 },
-        { name:"Miyuki", age:17 },
-        { name:"Lina", age:17 },
-        { name:"X", age:undefined }
-      ]
-    })
-  }
+    const [ otherState, updatedOtherState] = useState("some other value");
 
-  render() {
+    console.log(personsState, otherState);
+
+    const switchNameHandler = () => {
+      // console.log("name changed");
+      // Don't do this: this.state.persons[0].name = "Shiba Tatsuya";
+      updatedPersonsState({
+        persons: [
+          { name:"Shiba Tatsuya", age:18 },
+          { name:"Miyuki", age:17 },
+          { name:"Lina", age:17 },
+          { name:"X", age:undefined }
+        ],
+        // otherState: personsState.otherState
+      });
+    };
+
     return (
       <div className="App">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <h1 className="App-title">Welcome to React</h1>
         <p>I'm React App</p>
         <p>I'm learning React</p>
-        <button style={{color:'blue'}} onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >I love Shiba Tatuya</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} >I'm Half American Half Japanese</Person>
-        <Person name={this.state.persons[3].name} age={this.state.persons[3].age} />
+        <button style={{color:'blue'}} onClick={switchNameHandler}>Switch Name</button>
+        <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}  click={switchNameHandler}>I love Shiba Tatuya</Person>
+        <Person name={personsState.persons[2].name} age={personsState.persons[2].age} >I'm Half American Half Japanese</Person>
+        <Person name={personsState.persons[3].name} age={personsState.persons[3].age} />
         {/* <Person name="Miyuki" age="17" />
         <Person name="Lina" age="17" >I'm Half American Half Japanese</Person>
         <Person name="X" age="Unknown" /> */}
@@ -52,7 +59,7 @@ class App extends Component {
       </div>
     );
     // return React.createElement('div', {className:"App"}, React.createElement('h1',null,  'I\'m React App'));
-  }
 }
 
 export default App;
+
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,7 @@ import React from "react"
 const person = (props) => {
     return( 
         <div>
-            <p>I'm {props.name} and I'm {props.age} years old. My lucky number is {Math.floor(Math.random() * 30)}</p>
+            <p onClick={props.click}>I'm {props.name} and I'm {props.age} years old. My lucky number is {Math.floor(Math.random() * 30)}</p>
             <p>{props.children}</p>
         </div>
     )
