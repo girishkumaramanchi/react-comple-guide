142fc3f924404e5fdc6bccaab42b4266fdf4aee8
Girish <girishkumaramanchi@gmail.com>
2021-01-08 12:12:51 +0530

    Added setstate

--- src/App.js
+++ src/App.js
@@ -4,6 +4,28 @@ import Person from "./Person/Person"
 import './App.css';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name:"Tatsuya", age:18 },
+      { name:"Miyuki", age:17 },
+      { name:"Lina", age:17 },
+      { name:"X", age:undefined }
+    ]
+  }
+
+  switchNameHandler = () => {
+    // console.log("name changed");
+    // Don't do this: this.state.persons[0].name = "Shiba Tatsuya";
+    this.setState({
+      persons: [
+        { name:"Shiba Tatsuya", age:18 },
+        { name:"Miyuki", age:17 },
+        { name:"Lina", age:17 },
+        { name:"X", age:undefined }
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,11 +33,14 @@ class App extends Component {
         <h1 className="App-title">Welcome to React</h1>
         <p>I'm React App</p>
         <p>I'm learning React</p>
-        <button style={{color:'blue'}}>Switch Name</button>
-        <Person name="Tatsuya" age={18} />
-        <Person name="Miyuki" age="17" />
+        <button style={{color:'blue'}} onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >I love Shiba Tatuya</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} >I'm Half American Half Japanese</Person>
+        <Person name={this.state.persons[3].name} age={this.state.persons[3].age} />
+        {/* <Person name="Miyuki" age="17" />
         <Person name="Lina" age="17" >I'm Half American Half Japanese</Person>
-        <Person name="X" age="Unknown" />
+        <Person name="X" age="Unknown" /> */}
         {/* <Person></Person> */}
         {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
